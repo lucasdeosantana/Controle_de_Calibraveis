56195cff307f43ad454970e47e3c12b7a2dc41b5
TheRobt <ldos@N-VQT-04005.viaquatro.com.br>
2020-05-02 19:41:07 -0300

    bug no git

---
+++ CdC/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+stations = (
+    ("Patio","Patio"),
+    ("Morumbi","Morumbi"),
+    ("Butantã","Butantã"),
+    ("Pinheiros","Pinheiros"),
+    ("Faria Lima","Faria Lima"),
+    ("Fradique Coutinho","Fradique Coutinho"),
+    ("Oscar Freire","Oscar Freire"),
+    ("Paulista","Paulista"),
+    ("Higienopolis","Higienopolis"),
+    ("Republica","Republica"),
+    ("Luz","Luz"),
+    ("Calibração","Calibração"),
+    ("Base","Base")
+)
+stationscar = (
+    ("Patio","Patio"),
+    ("Morumbi","Morumbi"),
+    ("Butantã","Butantã"),
+    ("Pinheiros","Pinheiros"),
+    ("Faria Lima","Faria Lima"),
+    ("Fradique Coutinho","Fradique Coutinho"),
+    ("Oscar Freire","Oscar Freire"),
+    ("Paulista","Paulista"),
+    ("Higienopolis","Higienopolis"),
+    ("Republica","Republica"),
+    ("Luz","Luz"),
+    ("Calibração","Calibração"),
+    ("Em uso","Em uso")
+)
+# Create your models here.
+class log(models.Model):
+    codigo = models.IntegerField()
+    origem = models.CharField(max_length=30,blank=False,null=False,choices=stations)
+    destino = models.CharField(max_length=30, blank=False, null=False,choices=stations)
+    date = models.DateTimeField(auto_now_add=True)
+    responsible = models.CharField(max_length=10, blank=False, null=False)
+    observation = models.CharField(max_length=100, blank=True, null=True)
+
+class carlog(models.Model):
+    placa = models.CharField(max_length=30, blank=True,null=True)
+    origem = models.CharField(max_length=30,blank=False,null=False,choices=stationscar)
+    destino = models.CharField(max_length=30, blank=False, null=False,choices=stationscar)
+    date = models.DateTimeField(auto_now_add=True)
+    responsible = models.CharField(max_length=10, blank=False, null=False)
+
+class Equipament(models.Model):
+    codigo = models.IntegerField('Codigo do Equipamento',null=False,blank=False, unique=True)
+    nome = models.CharField('Nome', max_length=244, null=False, blank=False)
+    apelido = models.CharField('Apelido', max_length=100, blank=True, null=True)
+    position = models.CharField('Localização', max_length=30, blank=True, null=True,choices=stations)
+    date_calibration = models.DateField('Data de Calibração', blank=False, null=False)
+    date_validity=models.DateField('Data de validade, se prenche com 1 ano se não informado',blank=True, null=True)
+    in_calibration=models.IntegerField()
+
+    def __str__(self):
+        return str(self.codigo) + " " + self.nome
+
+    def save(self, *args, **kwargs):
+        if(self.date_validity == None):
+            self.date_validity = self.date_calibration+timedelta(days=365)
+        super(Equipament, self).save(*args, **kwargs)
+    def save_special(self):
+        self.date_validity = self.date_calibration+timedelta(days=365)
+        self.save()
+    class Meta:
+        permissions=[ 
+            ('can_move','Pode mover equipamentos'),
+            ('can_receive', 'Pode receber equipamentos de calibração'),
+            ('can_see_log', 'Pode ver os logs')
+        ]
+class Car(models.Model):
+    placa = models.CharField('Placa',max_length=8, blank=False, null=False, unique=True)
+    nome = models.CharField('Modelo',max_length=100, blank=False, null=False)
+    position = models.CharField('Localização',max_length=30, null=True, blank=True,choices=stationscar)
+    in_use =models.CharField('Utilizador', max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.placa + " " + self.nome
---
+++ CdC/views/list_of_equips.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.template import Context, Template
+from django.utils.timezone import datetime
+from django.utils.dateparse import parse_date
+import json
+from CdC.models import *
+class list_of_equips(PermissionRequiredMixin, View):
+    template_name = "login.html"
+    permission_required = 'CdC.can_move'
+    def get(self, request, station, *args, **Kwargs):
+        self.urlshort ={
+            "PVS":"Patio",
+            "MBI":"Morumbi",
+            "BUT":"Butantã",
+            "PIN":"Pinheiros",
+            "FAL":"Faria Lima",
+            "FRA":"Fradique Coutinho",
+            "FRE":"Oscar Freire",
+            "PTA":"Paulista",
+            "HIG":"Higienopolis",
+            "REP":"Republica",
+            "LUZ":"Luz",
+            "BASE":"Base"
+            }
+        equipmentList = (Equipament.objects.all().filter(position=self.urlshort[station])).order_by('date_validity')
+        print(equipmentList)
+        context={
+                "equipments":equipmentList,
+                "where":self.urlshort[station]
+                }
+        return render(request, 'equipment.html', context)
