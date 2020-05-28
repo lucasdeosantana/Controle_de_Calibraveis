27968b3c6de5f0800ebde0ca755d7bbfacecf7bd
Lucas de O Santana <lucasdeosantana@gmail.com>
2020-05-28 15:43:48 -0300

    clean master because develop is all diferent

--- CdC/views/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Cars import Cars
-from .do_login import do_login
-from .do_logout import do_logout
-from .list_of_equips import list_of_equips
-from .move import move
-from .superview import superview
